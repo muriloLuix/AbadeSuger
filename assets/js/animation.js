f6a95f25d7f6fc73abd467105b7bd9d8128ea817
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-05-16 21:28:38 -0300

    Atualizando carrossel
    Adicionado banner/carrossel para divulgar livros;
    Adicionando transitions para ser mais suave;

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -9,3 +9,37 @@ const elemento = document.querySelector('.general');
 setTimeout(() => {
     elemento.classList.add('animate');
 }, 2550);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bannerInner = document.querySelector(".banner-inner");
+  const bannerItems = document.querySelectorAll(".banner-item");
+  const totalItems = bannerItems.length;
+  let index = 0;
+  let interval = setInterval(showNextSlide, 5000);
+
+  function showNextSlide() {
+    index = (index + 1) % totalItems;
+    updateBanner();
+  }
+
+  function showPrevSlide() {
+    index = (index - 1 + totalItems) % totalItems;
+    updateBanner();
+  }
+
+  function updateBanner() {
+    bannerInner.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  document.querySelector(".next").addEventListener("click", () => {
+    clearInterval(interval);
+    showNextSlide();
+    interval = setInterval(showNextSlide, 5000);
+  });
+
+  document.querySelector(".prev").addEventListener("click", () => {
+    clearInterval(interval);
+    showPrevSlide();
+    interval = setInterval(showNextSlide, 5000);
+  });
+});
