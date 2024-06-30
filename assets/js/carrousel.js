2447272f2d4b7eb910752387012c9dff362deb23
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-06-30 00:53:39 -0300

    Finalizando tela inicial!

--- assets/js/carrousel.js
+++ assets/js/carrousel.js
@@ -1,34 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const carouselSlide = document.querySelector(".carousel-slide");
-    const carouselItems = document.querySelectorAll(".carousel-item");
-  
-    // Configurações iniciais
-    let counter = 0;
-    const totalItems = carouselItems.length;
-    const slideWidth = carouselSlide.clientWidth; // Largura de um slide
-  
-    // Ajusta a largura do slide conforme o número de itens
-    carouselSlide.style.width = `${totalItems * slideWidth}px`;
-  
-    // Botões de navegação
-    const prevBtn = document.querySelector(".carousel-prev");
-    const nextBtn = document.querySelector(".carousel-next");
-  
-    // Navegação para o slide anterior
-    prevBtn.addEventListener("click", () => {
-      counter--;
-      if (counter < 0) {
-        counter = totalItems - 1;
-      }
-      carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
-    });
-  
-    // Navegação para o próximo slide
-    nextBtn.addEventListener("click", () => {
-      counter++;
-      if (counter >= totalItems) {
-        counter = 0;
-      }
-      carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselSlide = document.querySelector(".carousel-slide");
+  const carouselItems = document.querySelectorAll(".carousel-item");
+  const totalItems = carouselItems.length;
+  const slideWidth = carouselSlide.clientWidth;
+  let counter = 0;
+  const slideInterval = 1000 * 5;
+
+  carouselSlide.style.width = `${totalItems * slideWidth}px`;
+
+  function nextSlide() {
+    counter++;
+    if (counter >= totalItems) {
+      counter = 0;
+    }
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
+  }
+
+  setInterval(nextSlide, slideInterval);
+
+  const prevBtn = document.querySelector(".carousel-prev");
+  const nextBtn = document.querySelector(".carousel-next");
+
+  prevBtn.addEventListener("click", () => {
+    counter--;
+    if (counter < 0) {
+      counter = totalItems - 1;
+    }
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
+  });
+
+  nextBtn.addEventListener("click", () => {
+    nextSlide();
+  });
 });
